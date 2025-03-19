0c945445f0b0931f470dcf3dcbea621442dc6322
janomaciell <janomaciel1@gmail.com>
2025-03-19 00:34:31 -0300

    change produccion

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Configurar interceptor para añadir token a las peticiones
 axios.interceptors.request.use(
