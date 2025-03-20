f3431c9992d5eff6b9ee9a873646d74e0cf48a5b
janomaciell <janomaciel1@gmail.com>
2025-03-20 15:53:05 -0300

    urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("¡API funcionando!"), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Agrega rutas para servir archivos media en desarrollo
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
