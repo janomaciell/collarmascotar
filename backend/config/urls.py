4232f4893155f2c9ea79e0e4ab6c6149a017a891
janomaciell <janomaciel1@gmail.com>
2025-04-15 23:02:38 -0300

    media en urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,19 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.views.static import serve
 
 urlpatterns = [
     path('', lambda request: HttpResponse("¡API funcionando!"), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
+# Los archivos estáticos los maneja whitenoise en producción
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
